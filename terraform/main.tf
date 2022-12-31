a767fb9acd1aa4c64454a7da63ec7fe6a407c36c
tompisula <tompisula@labrats.work>
2022-12-31 10:12:18 +0100

    use common modules, version pinning

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 
 module "networks" {
   for_each = local.config_networks
-  source   = "git::https://github.com/labrats-work/modules-terraform.git//modules/hetzner/network?ref=main"
+  source   = "git::https://github.com/labrats-work/modules-terraform.git//modules/hetzner/network?ref=1.0.2"
 
 
   network_name          = each.value.name
@@ -15,9 +15,15 @@ data "hetznerdns_zone" "dns_zone" {
 }
 
 module "node_group" {
-  source     = "./modules/node_group"
-  nodes      = local.config_nodes
-  public_key = var.public_key
+  source = "git::https://github.com/labrats-work/modules-terraform.git//modules/hetzner/node_group?ref=1.0.2"
+  nodes  = local.config_nodes
+  public_keys = [
+    var.public_key
+  ]
+  sshd_config = {
+    ssh_user = "sysadmin"
+    ssh_port = "2222"
+  }
   networks_map = { for config_network in local.config_networks : config_network.id =>
     {
       name       = config_network.id,
@@ -30,7 +36,7 @@ resource "hetznerdns_record" "dns_record" {
   for_each = module.node_group.nodes
 
   zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = replace("*.${each.value.name}",".labrats.work","")
+  name    = replace("*.${each.value.name}", ".labrats.work", "")
   value   = each.value.ipv4_address
   type    = "A"
   ttl     = 60
